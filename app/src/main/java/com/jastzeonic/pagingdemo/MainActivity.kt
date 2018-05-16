f139d3700e3bec8b0687e80817c1d9e9f35a14df
Jast <jastpower884@gmail.com>
2018-05-17 02:49:20 +0800

    Make page list sample enough to demo.

--- app/src/main/java/com/jastzeonic/pagingdemo/MainActivity.kt
+++ app/src/main/java/com/jastzeonic/pagingdemo/MainActivity.kt
@@ -1,32 +1,70 @@
 package com.jastzeonic.pagingdemo
 
+import android.arch.lifecycle.Observer
+import android.arch.paging.LivePagedListBuilder
+import android.arch.paging.PagedList
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import com.jastzeonic.pagingdemo.api.RedditApi
+import com.jastzeonic.pagingdemo.paging.SubRedditDataSourceFactory
 import com.jastzeonic.pagingdemo.vo.RedditPost
+import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
+
+    private val NETWORK_IO = Executors.newFixedThreadPool(5)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val call: Call<RedditApi.ListingResponse> = RedditApi.create().getTop("androidDev", 30)
-        call.enqueue(object : Callback<RedditApi.ListingResponse> {
-            override fun onFailure(call: Call<RedditApi.ListingResponse>?, t: Throwable?) {
-            }
 
-            override fun onResponse(call: Call<RedditApi.ListingResponse>?, response: Response<RedditApi.ListingResponse>?) {
+        val redditApi = RedditApi.create()
 
-                Log.d("Test", "test：" + response?.body()?.data?.children?.size)
+        val pageSize = 30
 
-            }
 
+        val sourceFactory = SubRedditDataSourceFactory(redditApi, "androidDev", NETWORK_IO)
+        val pagedListConfig = PagedList.Config.Builder()
+                .setEnablePlaceholders(false)
+                .setInitialLoadSizeHint(pageSize * 2)
+                .setPageSize(pageSize)
+                .build()
+        val pagedList = LivePagedListBuilder(sourceFactory, pagedListConfig)
+                // provide custom executor for network requests, otherwise it will default to
+                // Arch Components' IO pool which is also used for disk access
+                .setFetchExecutor(NETWORK_IO)
+                .build()
+
+        val adapter = PostsAdapter({
         })
+
+        val list = findViewById<RecyclerView>(R.id.recyclerView)
+
+        list.adapter = adapter
+        list.layoutManager = LinearLayoutManager(this)
+
+        pagedList.observe(this, Observer<PagedList<RedditPost>> {
+            adapter.submitList(it)
+        })
+
+
+//                call . enqueue (object : Callback<RedditApi.ListingResponse> {
+//            override fun onFailure(call: Call<RedditApi.ListingResponse>?, t: Throwable?) {
+//            }
+//
+//            override fun onResponse(call: Call<RedditApi.ListingResponse>?, response: Response<RedditApi.ListingResponse>?) {
+//
+//                Log.d("Test", "test：" + response?.body()?.data?.children?.size)
+//
+//            }
+//
+//        })
 
 
     }
---
+++ app/src/main/java/com/jastzeonic/pagingdemo/PostsAdapter.kt
@@ -0,0 +1,75 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.jastzeonic.pagingdemo
+
+import android.arch.paging.PagedListAdapter
+import android.support.v7.util.DiffUtil
+import android.support.v7.widget.RecyclerView
+import android.view.ViewGroup
+import com.jastzeonic.pagingdemo.vo.RedditPost
+
+/**
+ * A simple adapter implementation that shows Reddit posts.
+ */
+class PostsAdapter(private val retryCallback: () -> Unit)
+    : PagedListAdapter<RedditPost, RecyclerView.ViewHolder>(POST_COMPARATOR) {
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        (holder as RedditPostViewHolder).bind(getItem(position))
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int, payloads: MutableList<Any>) {
+        if (payloads.isNotEmpty()) {
+            val item = getItem(position)
+            (holder as RedditPostViewHolder).updateScore(item)
+        } else {
+            onBindViewHolder(holder, position)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return RedditPostViewHolder.create(parent)
+    }
+
+
+
+
+    companion object {
+        private val PAYLOAD_SCORE = Any()
+        val POST_COMPARATOR = object : DiffUtil.ItemCallback<RedditPost>() {
+            override fun areContentsTheSame(oldItem: RedditPost, newItem: RedditPost): Boolean =
+                    oldItem == newItem
+
+            override fun areItemsTheSame(oldItem: RedditPost, newItem: RedditPost): Boolean =
+                    oldItem.name == newItem.name
+
+            override fun getChangePayload(oldItem: RedditPost, newItem: RedditPost): Any? {
+                return if (sameExceptScore(oldItem, newItem)) {
+                    PAYLOAD_SCORE
+                } else {
+                    null
+                }
+            }
+        }
+
+        private fun sameExceptScore(oldItem: RedditPost, newItem: RedditPost): Boolean {
+            // DON'T do this copy in a real app, it is just convenient here for the demo :)
+            // because reddit randomizes scores, we want to pass it as a payload to minimize
+            // UI updates between refreshes
+            return oldItem.copy(score = newItem.score) == newItem
+        }
+    }
+}
